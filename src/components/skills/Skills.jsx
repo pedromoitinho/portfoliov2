8ca0670f1abda822d44a6d96a833c6a56fb35c54
Pedro Moitinho <pedromoitinhoprado@gmail.com>
2025-05-19 11:18:57 -0300

    Atualizar o Skills.jsx

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -26,7 +26,7 @@ const Skills = () => {
 
   return (
     <section id="skills" className="skills">
-      <h2>Minhas habilidades</h2>
+      <h2>Habilidades</h2>
       <div className="skills-container">
         {skillCategories.map((category, index) => (
           <div key={index} className="skill-category">
